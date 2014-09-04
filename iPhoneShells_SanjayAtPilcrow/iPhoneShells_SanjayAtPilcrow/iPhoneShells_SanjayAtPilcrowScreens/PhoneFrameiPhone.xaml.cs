a4436a0ef0860b6b8b1e51b11bd47b6d1ff0d476
sanjayatpilcrow <shosansharma@hotmail.com>
2014-09-04 18:16:32 +0530

    iPhone SketchFlow Skeleton For Prototype
    iPhone SketchFlow Skeleton For Prototype

---
+++ iPhoneShells_SanjayAtPilcrow/iPhoneShells_SanjayAtPilcrow/iPhoneShells_SanjayAtPilcrowScreens/PhoneFrameiPhone.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace iPhoneShells_SanjayAtPilcrowScreens
+{
+	/// <summary>
+	/// Interaction logic for PhoneFrameiPhone.xaml
+	/// </summary>
+	public partial class PhoneFrameiPhone : UserControl
+	{
+		public PhoneFrameiPhone()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
